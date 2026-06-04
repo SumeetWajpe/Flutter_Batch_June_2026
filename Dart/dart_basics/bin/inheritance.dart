b5e7f1042ad6eb1834853e2f50bd08146b9a691d
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-04 12:42:29 +0530

    ✨ Inheritance

---
+++ Dart/dart_basics/bin/inheritance.dart
@@ -0,0 +1,26 @@
+void main() {
+  JamesBondCar jbc = JamesBondCar(speed: 300, name: "Aston Martin");
+  // print(jbc.name);
+  print(jbc.accelerate());
+}
+
+class Car {
+  final String? name;
+  final double? speed;
+  Car(this.name, this.speed);
+
+  String accelerate() {
+    return ("The car $name is running at $speed kmph ");
+  }
+}
+
+class JamesBondCar extends Car {
+  bool? isArmed;
+  JamesBondCar({String name = "AUDI", double speed = 200, this.isArmed = false})
+    : super(name, speed);
+
+  @override
+  String accelerate() {
+    return "${super.accelerate()}  Is it armed ? : $isArmed";
+  }
+}
