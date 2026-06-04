261949ab03593eeba5c22b8346d6c63c89dedbd9
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-04 10:34:09 +0530

    ✨ Flutter basic - var | const | final

---
+++ Dart/dart_basics/bin/dart_basics.dart
@@ -0,0 +1,31 @@
+import 'package:dart_basics/dart_basics.dart' as dart_basics;
+
+void main(List<String> arguments) {
+  var name = "Dart"; // Type inference
+  // name = 10; // Error ~
+
+  String s;
+
+  /// declaration
+  s = "Flutter !"; // definition
+  s = "Flutter Programming !";
+  // s = null; // Errror !
+
+  String? str = "Hello"; // Nullable types
+  str = null;
+
+  // const
+  // const PI = 3.14;
+  // PI = 3.145445;//Error
+
+  // const PI; // Error (must initialize constants)
+
+  // final someVal;
+  // someVal = 20;
+  // someVal = 30; // Error
+
+  // Difference - Final is a runtime consatnt & const is a compilte time constant
+  final d = DateTime.now();
+  // 100 lines of code
+  // d = DateTime.now(); // Error
+}
