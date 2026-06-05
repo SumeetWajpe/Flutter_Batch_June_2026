3d9d639717270240b66164178f4b33ce3ff34c81
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-05 10:52:17 +0530

    ✨ Using Center

--- hello_flutter_app/lib/container.dart
+++ hello_flutter_app/lib/container.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 class MyContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
-      height: 150,
-      width: 150,
-      margin: EdgeInsets.all(20),
-      padding: EdgeInsets.all(10),
-      child: Text("Hello Container !"),
+    return Center(
+      child: Container(
+        color: Colors.amber,
+        height: 150,
+        width: 150,
+        margin: EdgeInsets.all(20),
+        padding: EdgeInsets.all(10),
+        child: Center(child: Text("Hello Container !")),
+      ),
     );
   }
 }
