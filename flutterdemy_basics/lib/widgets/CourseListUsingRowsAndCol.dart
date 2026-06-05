0260ad650619ac851d1c253809258a22f9932c8d
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-05 13:00:39 +0530

    ✨ Showing one course item in UI

--- flutterdemy_basics/lib/widgets/CourseListUsingRowsAndCol.dart
+++ flutterdemy_basics/lib/widgets/CourseListUsingRowsAndCol.dart
@@ -4,8 +4,14 @@ class CourseListWithRowsAndCols extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         Image(image: AssetImage("assets/images/react.png"), width: 100),
+        Text(
+          "React",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        Text("Delete", style: TextStyle(color: Colors.red)),
       ],
     );
   }
