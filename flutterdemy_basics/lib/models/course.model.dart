cfd2ea4b923348af19357260789af08211761672
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-08 10:29:50 +0530

    ✨ Adding CourseModel

---
+++ flutterdemy_basics/lib/models/course.model.dart
@@ -0,0 +1,8 @@
+class CourseModel {
+  final int id;
+  String? title;
+  final String? subtitle;
+  final int? likes;
+  final String? imageUrl;
+  CourseModel(this.id, this.title, this.subtitle, this.likes, this.imageUrl);
+}
