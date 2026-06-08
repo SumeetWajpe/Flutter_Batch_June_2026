9154c00062370c3f46ae006a48e0df5341e46235
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-08 09:59:30 +0530

    ✨ Using SingleChildScrollView

--- flutterdemy_basics/lib/widgets/CourseListUsingRowsAndCol.dart
+++ flutterdemy_basics/lib/widgets/CourseListUsingRowsAndCol.dart
@@ -3,16 +3,209 @@ import 'package:flutter/material.dart';
 class CourseListWithRowsAndCols extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Image(image: AssetImage("assets/images/react.png"), width: 100),
-        Text(
-          "React",
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-        ),
-        Text("Delete", style: TextStyle(color: Colors.red)),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image(image: AssetImage("assets/images/react.png"), width: 100),
+              Text(
+                "React",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text("Delete", style: TextStyle(color: Colors.red)),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
