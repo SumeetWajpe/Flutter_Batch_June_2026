74a0a76d43d10cf3e2f860e7be3bb357484c0925
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-10 11:55:40 +0530

    ✨ Anvigating to about Page

--- flutter_app_go_router_basics/lib/pages/home.dart
+++ flutter_app_go_router_basics/lib/pages/home.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Center(child: Text("Home")),
+          ElevatedButton(
+            onPressed: () {
+              GoRouter.of(context).push(Uri(path: "/about").toString());
+            },
+            child: Text("Go to About"),
+          ),
+        ],
+      ),
+    );
   }
 }
