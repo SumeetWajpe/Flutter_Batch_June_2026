d43537f15821d090f90bb7135122cd1a90cd1643
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-11 12:41:29 +0530

    ✨ Converted JSOn to PostModel

--- flutter_application_future_posts/lib/postlist.dart
+++ flutter_application_future_posts/lib/postlist.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_future_posts/posts.mode.dart';
 import 'package:http/http.dart' as http;
 
 class PostListScreen extends StatefulWidget {
@@ -12,7 +15,13 @@ class _PostListScreenState extends State<PostListScreen> {
   void fetchPosts() async {
     final url = Uri.parse("https://jsonplaceholder.typicode.com/posts");
     final response = await http.get(url);
-    print(response.statusCode);
+    // print(response.statusCode);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = json.decode(response.body);
+      var posts = jsonList.map((json) => PostModel.fromJson(json)).toList();
+      print(posts.length);
+    }
   }
 
   @override
