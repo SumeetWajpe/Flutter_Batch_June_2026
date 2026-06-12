1b6366b7f3a73af361bed66592e5cf070917ad32
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-12 12:39:06 +0530

    ✨ Configured BlocProvider | BlocBuilder | UI

---
+++ flutterdemy_bloc/lib/widgets/course.widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutterdemy_bloc/models/course.model.dart';
+
+class CourseWidget extends StatelessWidget {
+  final CourseModel course;
+  const CourseWidget({super.key, required this.course});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 15,
+      margin: EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        leading: Image(image: NetworkImage(course.imageUrl!), width: 100),
+        title: Text(
+          course.title!,
+          style: const TextStyle(
+            fontSize: 20,
+            color: Color.fromARGB(255, 17, 17, 17),
+          ),
+        ),
+        subtitle: Text(
+          course.subtitle!,
+          style: const TextStyle(fontSize: 15, color: Colors.grey),
+        ),
+        trailing: InkWell(
+          child: const Icon(
+            Icons.delete,
+            color: Color.fromARGB(255, 237, 79, 68),
+          ),
+          onTap: () {},
+        ),
+      ),
+    );
+  }
+}
