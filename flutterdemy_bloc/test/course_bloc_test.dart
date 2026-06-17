3dfea865721f9bf8b62b0e68e3c60a3a8e08e79a
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-17 12:34:28 +0530

    ✨ Testing using blocTest

--- flutterdemy_bloc/test/course_bloc_test.dart
+++ flutterdemy_bloc/test/course_bloc_test.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutterdemy_bloc/bloc/course.bloc.dart';
+import 'package:flutterdemy_bloc/models/course.model.dart';
 
 void main() {
   group('Course Bloc Tests suite', () {
@@ -17,5 +19,33 @@ void main() {
     test('initial State contains 4 courses', () {
       expect(bloc.state.courses.length, 4);
     });
+
+    // blocTest -> build, act , verify
+    blocTest<CourseBloc, CourseState>(
+      'AddCourse Event adds a new course',
+      build: () => CourseBloc(),
+      act: (bloc) {
+        bloc.add(
+          AddCourseEvent(
+            CourseModel(
+              5,
+              "Vue3",
+              "A progressive Framework",
+              200,
+              "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+              "Vue3 is an open-source UI software development kit (SDK) created by Google that allows developers to build natively compiled applications for mobile, web, and desktop from a single codebase. Flutter is an open-source UI software development kit (SDK) created by Google that allows developers to build natively compiled applications for mobile, web, and desktop from a single codebase. ",
+              5,
+              DateTime.now().subtract(const Duration(days: 10)),
+              "English",
+              ["English", "German", "Spanish"],
+              "Aman Jha",
+            ),
+          ),
+        );
+      },
+      verify: (bloc) {
+        expect(bloc.state.courses.length, 5);
+      },
+    );
   });
 }
